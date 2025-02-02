eca2f37c328c188e9da82dd13b1e81d0be993695
Stanislav <s.zaporozhan97@mail.ru>
2025-02-02 21:21:13 +0600

    после rubocop

--- app/controllers/api/v1/banks/users/cards_controller.rb
+++ app/controllers/api/v1/banks/users/cards_controller.rb
@@ -2,16 +2,14 @@ class Api::V1::Banks::Users::CardsController < ApplicationController
   def index
     render json: { data: CardBlueprint.render_as_hash(cards) }
   end
-#создать контроллерр cards
-  def send_money #перенести в новый контроллер cards
-    if card.balance >= params[:amount]
-      Card.transaction do 
-        card.balance -= params[:amount]
-        card.save
 
-        card_to = Card.find_by(number: params[:card_number])
-        card_to.balance += params[:amount]
-        card_to.save
+  # создать контроллерр cards
+  # перенести в новый контроллер cards
+  def send_money
+    if card.balance >= params[:amount]
+      Card.transaction do
+        withdrawal
+        replenishment
       end
 
       head :ok
@@ -28,5 +26,16 @@ class Api::V1::Banks::Users::CardsController < ApplicationController
 
   def card
     @card ||= cards.find(params[:card_id])
+  end
+
+  def withdrawal
+    card.balance -= params[:amount]
+    card.save
+  end
+
+  def replenishment
+    card_to = Card.find_by(number: params[:card_number])
+    card_to.balance += params[:amount]
+    card_to.save
   end
 end
